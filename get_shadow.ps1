75ae15ce594fc9a09f18fb49a5d0221ab7b601f2
Denis Solomasov <dini@bk.ru>
2016-05-14 15:45:38 +0300

    Second commit

---
+++ get_shadow.ps1
@@ -0,0 +1,34 @@
+﻿# получить теневые копии
+function Get-ShadowCopy
+{
+    [CmdletBinding()]
+      param
+      (
+        [Parameter(
+         ValueFromPipeline=$True,
+         ValueFromPipelineByPropertyName=$True)]
+         [Alias('PSComputerName')] [string]$ComputerName = $env:COMPUTERNAME
+
+      )
+begin
+{
+    if ($PSBoundParameters.Verbose) {$VerbosePreference = "Continue"}# необходимо для Write-Verbose версия 2.0
+}
+process
+    {
+
+        $ShadowCopyes = gwmi win32_shadowcopy -comp $ComputerName;
+        $Volumes = gwmi win32_volume -comp $ComputerName | select DeviceID,DriveLetter;
+
+        $ShadowCopyes `
+        | select @{n="drive";e={
+                $id = $_.VolumeName;
+                $Volumes | where { $_.deviceID -eq $id} | select -ExpandProperty driveletter;
+            }}, 
+            @{n="date"; e = {
+                [datetime]::ParseExact($_.installDate.Split(".")[0], "yyyyMMddHHmmss", $null)
+            }},
+            DeviceObject
+    }
+end {}
+}
